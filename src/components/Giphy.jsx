a38eb950a5b60af1db7f5e2361d083dd13ae7f20
Alex Cyrus <alex@alexcyrus.com>
2021-09-21 12:16:48 -0400

    fetch data from giphy api

--- src/components/Giphy.jsx
+++ src/components/Giphy.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Giphy = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const results = await axios("https://api.giphy.com/v1/gifs/trending", {
+        params: {
+          api_key: "1XhJ0iwNaQzWMqoIoWlRraoe9Gxo89PE"
+        }
+      })
+
+      console.log(results);
+    }
+
+    fetchData();
+  })
+
   return (
   <div className="">
     Giphy
